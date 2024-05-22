4f01e0296d4eb0ee68340543ec515b4c658dc73d
Carlos Isaac Rodriguez Prado <carlos.rodriguez.prado@melia.com>
2024-05-22 12:48:21 +0200

    Added train arguments class

---
+++ material-groups-forecast/material_groups_forecast/argument/train_arguments.py
@@ -0,0 +1,15 @@
+import configparser
+from argparse import ArgumentParser
+from datetime import datetime
+
+
+class TrainArguments:
+    def __init__(self):
+        self.fmt = "%Y-%m-%d"
+
+        self.config = configparser.ConfigParser()
+
+        self.parser = ArgumentParser()
+        self.parser.add_argument("--execution_date", type=lambda d: datetime.strptime(d, self.fmt).date())
+        self.parser.add_argument("--start_date", type=lambda d: datetime.strptime(d, self.fmt).date())
+        self.parser.add_argument("--end_date", type=lambda d: datetime.strptime(d, self.fmt).date())
