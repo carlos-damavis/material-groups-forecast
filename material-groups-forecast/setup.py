4d709a1538631f81f74a0edf972fb03dd754d2f4
Carlos Isaac Rodriguez Prado <carlos.rodriguez.prado@melia.com>
2024-05-22 13:38:37 +0200

    Added setup and manifest files

---
+++ material-groups-forecast/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="material_groups_forecast",
+    version="0.1.0.dev0",
+    packages=find_packages(".", exclude=["tests", "tests.*"]),
+    url="",
+    license="",
+    install_requires=[
+        "pandas==2.2.2",
+        "scikit-learn==1.5.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "train_models=train_main:main"
+        ]
+    },
+    include_package_data=True,
+    author="Carlos Isaac Rodriguez Prado",
+    author_email="carlos.rodriguez@damavis.com"
+)
