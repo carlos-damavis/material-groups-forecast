7ecdc529570646570a54f1c8b464fd6742b58b80
Carlos Isaac Rodriguez Prado <carlos.rodriguez.prado@melia.com>
2024-05-23 15:46:37 +0200

    Moved forecastedGroup renaming to training data processor

--- material-groups-forecast/test/training/test_training_data_day_grouper.py
+++ material-groups-forecast/test/training/test_training_data_day_grouper.py
@@ -1,11 +1,12 @@
 import os
 import unittest
+from datetime import date
 
-from pandas import read_csv, ArrowDtype, to_datetime
+from pandas import read_csv, ArrowDtype
 from pandas.testing import assert_frame_equal
-from pyarrow import table
+from pyarrow import table, schema
 
-from material_groups_forecast.stage.common.groups_argument_mapper_impl import GroupsArgumentMapperImpl
+from material_groups_forecast.schema.processed_materials_loads_schema import processed_materials_loads_schema
 from material_groups_forecast.stage.training.training_data_day_grouper_impl import TrainingDataDayGrouperImpl
 from test import ASSETS_TEST_PATH
 
@@ -14,51 +15,51 @@ class TestTrainingDataDayGrouper(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        data_path = os.path.join(ASSETS_TEST_PATH, "training", "training_data_grouper")
-        groups_argument_mapper = GroupsArgumentMapperImpl()
-        cls.training_data_grouper_material = TrainingDataDayGrouperImpl(groups_argument_mapper, "material")
-        cls.training_data_grouper_budget = TrainingDataDayGrouperImpl(groups_argument_mapper, "budget")
-        cls.training_data_grouper_budget2 = TrainingDataDayGrouperImpl(groups_argument_mapper, "budget2")
-        data = read_csv(os.path.join(data_path, "training_processed_data_1.csv"), dtype_backend="pyarrow")
-        data.fecha_entrada = to_datetime(data.fecha_entrada)
-        cls.data = data
+        cls.data_path = os.path.join(ASSETS_TEST_PATH, "training", "training_data_grouper")
+        cls.training_data_grouper = TrainingDataDayGrouperImpl()
 
     def test_group_1(self):
+        data = read_csv(os.path.join(self.data_path, "training_processed_data_1.csv"), dtype_backend="pyarrow")
+        data = table(data, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
 
         expected = table(
             {
-                "grupo": ["RCD", "RECHAZO ENVASES", "RS GII", "RU", "RU"],
-                "fecha_entrada": ["2005-03-17", "2005-03-10", "2005-03-28", "2005-03-12", "2005-03-26"],
+                "forecastedGroup": ["RCD", "RECHAZO ENVASES", "RS GII", "RU", "RU"],
+                "fecha_entrada": [date(2005, 3, 17), date(2005, 3, 10), date(2005, 3, 28),
+                                  date(2005, 3, 12), date(2005, 3, 26)],
                 "peso_neto": [4720, 1420, 660, 2680, 500]
-            }).to_pandas(types_mapper=ArrowDtype)
-        expected.fecha_entrada = to_datetime(expected.fecha_entrada)
+            }, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
 
-        result = self.training_data_grouper_material.group(self.data)
+        result = self.training_data_grouper.group(data)
 
         assert_frame_equal(result, expected)
 
     def test_group_2(self):
+        data = read_csv(os.path.join(self.data_path, "training_processed_data_2.csv"), dtype_backend="pyarrow")
+        data = table(data, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
+
         expected = table(
             {
-                "grupo_presupuestos": ["PVE", "PVE"],
-                "fecha_entrada": ["2005-03-10", "2005-03-26"],
+                "forecastedGroup": ["PVE", "PVE"],
+                "fecha_entrada": [date(2005, 3, 10), date(2005, 3, 26)],
                 "peso_neto": [1420, 1480]
-            }).to_pandas(types_mapper=ArrowDtype)
-        expected.fecha_entrada = to_datetime(expected.fecha_entrada)
+            }, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
 
-        result = self.training_data_grouper_budget.group(self.data)
+        result = self.training_data_grouper.group(data)
 
         assert_frame_equal(result, expected)
 
     def test_group_3(self):
+        data = read_csv(os.path.join(self.data_path, "training_processed_data_3.csv"), dtype_backend="pyarrow")
+        data = table(data, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
+
         expected = table(
             {
-                "grupo_presupuestos_v2": ["PAPEL-EETT", "RU-PVE", "VIDRIO-GENERAL"],
-                "fecha_entrada": ["2005-03-12", "2005-03-26", "2005-03-07"],
+                "forecastedGroup": ["PAPEL-EETT", "RU-PVE", "VIDRIO-GENERAL"],
+                "fecha_entrada": [date(2005, 3, 12), date(2005, 3, 26), date(2005, 3, 7)],
                 "peso_neto": [3380, 500, 4440]
-            }).to_pandas(types_mapper=ArrowDtype)
-        expected.fecha_entrada = to_datetime(expected.fecha_entrada)
+            }, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
 
-        result = self.training_data_grouper_budget2.group(self.data)
+        result = self.training_data_grouper.group(data)
 
         assert_frame_equal(result, expected)
--- material-groups-forecast/test/training/test_training_data_processor.py
+++ material-groups-forecast/test/training/test_training_data_processor.py
@@ -1,11 +1,13 @@
 import os
 import unittest
 
-from pandas import read_csv, to_datetime, ArrowDtype
+from pandas import read_csv, ArrowDtype
 from pandas.testing import assert_frame_equal
 from pyarrow import table, schema
 
 from material_groups_forecast.schema.materials_loads_schema import materials_loads_schema
+from material_groups_forecast.schema.processed_materials_loads_schema import processed_materials_loads_schema
+from material_groups_forecast.stage.common.groups_argument_mapper_impl import GroupsArgumentMapperImpl
 from material_groups_forecast.stage.training.training_data_processor_impl import TrainingDataProcessorImpl
 from test import ASSETS_TEST_PATH
 
@@ -14,15 +16,40 @@ class TestTrainingDataProcessor(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.training_data_processor = TrainingDataProcessorImpl()
-        cls.data_path = os.path.join(ASSETS_TEST_PATH, "training", "training_data_processor")
+        cls.groups_argument_mapper = GroupsArgumentMapperImpl()
+        data_path = os.path.join(ASSETS_TEST_PATH, "training", "training_data_processor")
+        data = read_csv(os.path.join(data_path, "training_raw_data_1.csv"), dtype_backend="pyarrow")
+        cls.data = table(data, schema=schema(materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
 
     def test_process_1(self):
-        data = read_csv(os.path.join(self.data_path, "training_raw_data_1.csv"), dtype_backend="pyarrow")
-        data = table(data, schema=schema(materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
-        expected = data.copy()
-        expected.fecha_entrada = to_datetime(expected.fecha_entrada)
+        training_data_processor = TrainingDataProcessorImpl(self.groups_argument_mapper, "material")
 
-        result = self.training_data_processor.process(data)
+        expected = self.data.loc[:, ["grupo", "fecha_entrada", "peso_neto"]].copy()
+        expected.columns = ["forecastedGroup", "fecha_entrada", "peso_neto"]
+        expected = table(expected, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
+
+        result = training_data_processor.process(self.data)
+
+        assert_frame_equal(result, expected)
+
+    def test_process_2(self):
+        training_data_processor = TrainingDataProcessorImpl(self.groups_argument_mapper, "budget")
+
+        expected = self.data.loc[:, ["grupo_presupuestos", "fecha_entrada", "peso_neto"]].copy()
+        expected.columns = ["forecastedGroup", "fecha_entrada", "peso_neto"]
+        expected = table(expected, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
+
+        result = training_data_processor.process(self.data)
+
+        assert_frame_equal(result, expected)
+
+    def test_process_3(self):
+        training_data_processor = TrainingDataProcessorImpl(self.groups_argument_mapper, "budget2")
+
+        expected = self.data.loc[:, ["grupo_presupuestos_v2", "fecha_entrada", "peso_neto"]].copy()
+        expected.columns = ["forecastedGroup", "fecha_entrada", "peso_neto"]
+        expected = table(expected, schema=schema(processed_materials_loads_schema)).to_pandas(types_mapper=ArrowDtype)
+
+        result = training_data_processor.process(self.data)
 
         assert_frame_equal(result, expected)
